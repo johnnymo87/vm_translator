5fb4774d65ed91c1ccef8877527f8d1b76262751
johnnymo87 <johnnymo87@gmail.com>
2014-03-21 17:35:46 +0000

    init

---
+++ parser/parser_test.go
@@ -0,0 +1,11 @@
+package parser_test
+
+import (
+	. "github.com/johnnymo87/vm_translator/parser"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Parser", func() {
+
+})
---
+++ writer/writer_test.go
@@ -0,0 +1,11 @@
+package writer_test
+
+import (
+	. "github.com/johnnymo87/vm_translator/writer"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Writer", func() {
+
+})
